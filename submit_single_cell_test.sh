801e5dea50b281b0def00732112fcf238892b3e7
harrietgilmour <harrietgilmour12@gmail.com>
2023-09-05 15:26:08 +0100

    added in vert vel file

--- submit_single_cell_test.sh
+++ submit_single_cell_test.sh
@@ -8,12 +8,14 @@ mask_file=$1
 precip_file=$2
 tracks_file=$3
 tb_file=$4
-cell=$5
+w_file=$5
+cell=$6
 
 echo "$mask_file"
 echo "$precip_file"
 echo "$tracks_file"
 echo "$tb_file"
+echo "$w_file"
 echo "$cell"
 
-python single_cell_loop.py ${mask_file} ${precip_file} ${tracks_file} ${tb_file} ${cell}
+python single_cell_loop.py ${mask_file} ${precip_file} ${tracks_file} ${tb_file} ${w_file} ${cell}

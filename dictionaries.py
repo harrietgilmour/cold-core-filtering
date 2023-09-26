2d7b8999a62e1811dd39d892a4933c4d728ea84c
harrietgilmour <harrietgilmour12@gmail.com>
2023-09-26 10:36:54 +0100

    changed CC to 210K and added comments

--- dictionaries.py
+++ dictionaries.py
@@ -23,7 +23,8 @@ precip_threshold = 1 #mm/hr
 heavy_precip_threshold = 10 # mm/hr
 extreme_precip_threshold = 50 # mm/hr (based on Marengo, J. A., Ambrizzi, T., Alves, L. M., Barreto, N. J., Simões Reboita, M., & Ramos, A. M. (2020). Changing trends in rainfall extremes in the metropolitan area of São Paulo: causes and impacts. Frontiers in Climate, 2, 3.)
 
-precip_area = 25 # threshold for the minimum number of grid points that must be precipitating for a track to remain (and not be dropped from the tracks dataset)
+precip_area = 198 # threshold for the minimum number of grid points that must be precipitating for a track to remain (and not be dropped from the tracks dataset)
+                  # This value is equivalent to ~10% of the 40,000km2 cloud shield area requirement. 
 
 ## other parameters that need to be defined before loop ##
 s = generate_binary_structure(2,2) # need this in loop later on
@@ -32,4 +33,6 @@ removed_tracks = 0 # need this for loop later on
 precipitation_flag = 0
 cold_core_flag = 0
 
-cold_threshold = 200
+cold_threshold = 210 # 210K was chosen after sensitivity analysis for 200, 210 and 215K. 200K didn't allow for any MCSs to show in the La Plata Basin so was too 
+                     # restrictive. 215K produced too many over the ocean in south eastern south america that are more likely to be extratropical tracks so wasn't
+                     # restrictive enough. 210K allowed for MCSs to appear in the LPB but not over the ocean where the extratropical systems are likely to be

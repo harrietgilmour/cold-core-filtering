45e207aeb416a055df3e21d8875b707ae82b3106
harrietgilmour <harrietgilmour12@gmail.com>
2023-09-27 15:51:29 +0100

    updated save file paths

--- single_cell_loop.py
+++ single_cell_loop.py
@@ -642,12 +642,12 @@ def main():
     else:
         if cold_core_flag < 6:
             subset = subset.drop(subset[subset.cell == cell].index)
-            subset.to_hdf('/project/cssp_brazil/mcs_tracking_HG/final_tracks_CPM/deleted_tracks/cold_core/tracks_2005_01_cell_{}.hdf'.format(cell), 'table')
+            subset.to_hdf('/project/cssp_brazil/mcs_tracking_HG/final_tracks_CPM/2005/deleted_tracks/cold_core/tracks_2005_01_cell_{}.hdf'.format(cell), 'table')
         elif rain_flag < 6:
             subset = subset.drop(subset[subset.cell == cell].index)
-            subset.to_hdf('/project/cssp_brazil/mcs_tracking_HG/final_tracks_CPM/deleted_tracks/precip/tracks_2005_01_cell_{}.hdf'.format(cell), 'table')
+            subset.to_hdf('/project/cssp_brazil/mcs_tracking_HG/final_tracks_CPM/2005/deleted_tracks/precip/tracks_2005_01_cell_{}.hdf'.format(cell), 'table')
         else:
-            subset.to_hdf('/project/cssp_brazil/mcs_tracking_HG/final_tracks_CPM/CC&PF/tracks_2005_01_cell_{}.hdf'.format(cell), 'table')
+            subset.to_hdf('/project/cssp_brazil/mcs_tracking_HG/final_tracks_CPM/2005/CC&PF/tracks_2005_01_cell_{}.hdf'.format(cell), 'table')
             print('Saved file for cell {}'.format(cell))
  
 
